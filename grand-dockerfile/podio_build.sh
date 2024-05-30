eccced26f178ff10530ce7dc733bdf6b2fdbe9a4
tsaie79 <tsaie79@gmail.com>
2024-05-30 17:54:29 +0000

    Update Dockerfile to include CMD for running the application

--- grand-dockerfile/podio_build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -l
-#
-#
-git clone https://github.com/faustus123/podio -b davidl_RootReader_TDirectory podio.src
-export podio_ROOT=${PWD}/podio
-cmake -S podio.src -B podio.build -DCMAKE_INSTALL_PREFIX=${podio_ROOT} -DCMAKE_CXX_STANDARD=20
-cmake --build podio.build --target install -j16
-export LD_LIBRARY_PATH=${podio_ROOT}/lib:${LD_LIBRARY_PATH}
---
+++ grand-dockerfile/run-EICrecon.bash
@@ -0,0 +1,16 @@
+
+#!/bin/bash
+
+source /opt/detector/epic-main/bin/thisepic.sh
+source ./EICrecon/bin/eicrecon-this.sh
+source ${JANA_ROOT}/bin/jana-this.sh
+export podio_ROOT=${PWD}/podio
+export LD_LIBRARY_PATH=${podio_ROOT}/lib:${LD_LIBRARY_PATH}
+export SIMFILE=${PWD}/simout.100.edm4hep.root
+export JANA_ROOT=${PWD}/JANA2
+export LD_LIBRARY_PATH=${JANA_ROOT}/lib:${LD_LIBRARY_PATH}
+export JANA_PLUGIN_PATH=${PWD}/podiostream.build:${JANA_PLUGIN_PATH}
+
+export DETECTOR_PATH=/opt/detector/epic-main/share/epic
+
+eicrecon -Pplugins=podiostream podiostreamSource
