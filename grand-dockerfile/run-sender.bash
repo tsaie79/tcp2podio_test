4deb4fba8674253e269ffc5b9b2285c114e0567b
tsaie79 <tsaie79@gmail.com>
2024-05-30 21:31:08 +0000

    Refactor Dockerfile to include necessary environment variables and CMD for running the application

---
+++ grand-dockerfile/run-sender.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Convert the podio file into a file of pre-serialized buffers of 10 events each
+./podio2tcp.build/podio2tcp -o ${SIMFILE}.podiostr -g 10  $SIMFILE
+
+# Read from the newly created file
+./podio2tcp.build/podio2tcp --loop ${SIMFILE}.podiostr
