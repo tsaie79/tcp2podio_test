9d027fea38ba33fce3be55d1c0f78f297efd4fff
tsaie79 <tsaie79@gmail.com>
2024-05-30 21:34:10 +0000

    Refactor Dockerfile to include necessary environment variables and make run-sender.bash executable

--- grand-dockerfile/run-sender.bash
+++ grand-dockerfile/run-sender.bash
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+export SIMFILE=${PWD}/simout.100.edm4hep.root
+
 # Convert the podio file into a file of pre-serialized buffers of 10 events each
 ./podio2tcp.build/podio2tcp -o ${SIMFILE}.podiostr -g 10  $SIMFILE
 
